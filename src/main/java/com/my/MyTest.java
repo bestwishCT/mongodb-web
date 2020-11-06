e2c584d5f855d3926e0fd9a4b4a73d8579027085
bestwishCT <1758031883@qq.com>
2020-11-06 18:02:08 +0800

    spring 提交

--- src/main/java/com/my/MyTest.java
+++ src/main/java/com/my/MyTest.java
@@ -1,13 +1,19 @@
 package com.my;
 
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.xml.XmlBeanFactory;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.core.io.ClassPathResource;
 
 public class MyTest {
 	public static void main(String[] args) {
 //1
 //        ApplicationContext ctx=new ClassPathXmlApplicationContext("applicationContext.xml");
+		XmlBeanFactory factory = new XmlBeanFactory
+                (new ClassPathResource("applicationContext.xml"));
+		
 //        //基于XML配置方式
 //        SomeBean sb = ctx.getBean(SomeBean.class);
 //        System.out.println(sb);
@@ -20,6 +26,7 @@ public class MyTest {
 		  //体会下这个方法
 		  String str = ctx.getBean(String.class);
 		  System.out.println(str);
+		  //
 		  System.exit(0);
     
 	}
